d9d3b73abf5e07e07c5350c9d944ba9733db6bac
Peter Sjöqvist <peter@Peters-iMac.bredbandsbolaget.se>
2016-08-21 20:06:49 +0200

    Cleanup2 git push origin master

--- Plugin/PaymentSession/Init.php
+++ Plugin/PaymentSession/Init.php
@@ -42,8 +42,6 @@ class Init
      */
     public function beforeExecute(\Magento\Checkout\Controller\Index\Index $subject)
     {
-        return;
-
         // Initialize payment session.
         if (!$this->apiModel->paymentSessionInitialized()) {
             // Assign default address information to quote.
