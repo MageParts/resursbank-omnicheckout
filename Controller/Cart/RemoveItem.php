92c0e1b93bff63010931aafc7a631cb9e77961a6
Peter Sjöqvist <peter@Peters-iMac.bredbandsbolaget.se>
2016-08-22 13:01:29 +0200

    Fixed bugs when deleting items

--- Controller/Cart/RemoveItem.php
+++ Controller/Cart/RemoveItem.php
@@ -10,37 +10,8 @@ use Exception;
  * Class RemoveItem
  * @package Resursbank\OmniCheckout\Controller\Cart
  */
-class RemoveItem extends \Magento\Framework\App\Action\Action
+class RemoveItem extends \Magento\Checkout\Controller\Cart\Delete
 {
-    /**
-     * @var \Magento\Quote\Api\CartRepositoryInterface
-     */
-    private $quoteRepository;
-
-    /**
-     * @var \Resursbank\OmniCheckout\Helper\Api
-     */
-    private $apiHelper;
-
-    /**
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
-     * @param \Resursbank\OmniCheckout\Helper\Api $apiHelper
-     * @param \Magento\Framework\Controller\ResultFactory $resultFactory
-     */
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
-        \Resursbank\OmniCheckout\Helper\Api $apiHelper,
-        \Magento\Framework\Controller\ResultFactory $resultFactory
-    ) {
-        $this->quoteRepository = $quoteRepository;
-        $this->apiHelper = $apiHelper;
-        $this->resultFactory = $resultFactory;
-
-        parent::__construct($context);
-    }
-
     /**
      * Initializes payment session at Resursbank and renders the checkout page.
      *
@@ -49,20 +20,14 @@ class RemoveItem extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        // Get id of quote item to be deleted.
-        $id = (int) $this->getRequest()->getParam('id');
+        parent::execute();
 
-        if (!$id) {
-            throw new Exception('Please provide a valid item id.');
-        }
-
-        // Delete the item.
-        $this->apiHelper->getQuote()->removeItem($id);
-        $this->quoteRepository->save($this->apiHelper->getQuote());
+        $apiHelper = $this->_objectManager->create('\Resursbank\OmniCheckout\Helper\Api');
+        $resultFactory = $this->_objectManager->create('\Magento\Framework\Controller\ResultFactory');
 
         // Build response object.
-        $result = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_JSON);
-        $result->setData(['cart_qty' => $this->apiHelper->getQuote()->getItemsQty()]);
+        $result = $resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_JSON);
+        $result->setData(['cart_qty' => $apiHelper->getQuote()->getItemsQty()]);
 
         // Respond.
         return $result;
