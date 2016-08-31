6eb762b1a39d61b93ee67c57fbb0842e1591d0bb
Peter Sjöqvist <peter@Peters-iMac.bredbandsbolaget.se>
2016-08-31 21:10:39 +0200

    Added copyright notice

--- Helper/Callback.php
+++ Helper/Callback.php
@@ -1,15 +1,27 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Bossehasse
- * Date: 09/08/16
- * Time: 11:48
+ * Copyright 2016 Resurs Bank AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 namespace Resursbank\OmniCheckout\Helper;
 
 use Exception;
 
+/**
+ * Class Callback
+ * @package Resursbank\OmniCheckout\Helper
+ */
 class Callback extends \Magento\Framework\App\Helper\AbstractHelper
 {
     /**
@@ -92,10 +104,6 @@ class Callback extends \Magento\Framework\App\Helper\AbstractHelper
      */
     public function addOrderComment(\Magento\Sales\Model\Order $order, $comment)
     {
-        if (!is_string($comment)) {
-            throw new Exception('Comment must be strings.');
-        }
-
         $order->addStatusHistoryComment($comment, false);
 
         $this->orderRepository->save($order);
